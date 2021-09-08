b99b9a6438d1813fbc5ac01019e645d6381a62e7
Thom <hsbang.thom@gmail.com>
2021-09-08 13:33:20 +0900

    update address test code PUT ajax

--- src/test/java/com/ktx/ddep/controller/MypageApiControllerTest.java
+++ src/test/java/com/ktx/ddep/controller/MypageApiControllerTest.java
@@ -2,9 +2,9 @@ package com.ktx.ddep.controller;
 
 import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -15,11 +15,10 @@ import javax.servlet.http.HttpServletRequest;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -29,14 +28,15 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ktx.ddep.config.test.ApiTestConfig;
-import com.ktx.ddep.config.test.MvcTestConfig;
+import com.ktx.ddep.dto.member.Address;
 import com.ktx.ddep.dto.member.SessionUser;
 import com.ktx.ddep.dto.recipe.Rcp;
 import com.ktx.ddep.service.RecipesService;
 
 @RunWith(SpringRunner.class)
-@ContextConfiguration(classes = {ApiTestConfig.class, MvcTestConfig.class})
+@ContextConfiguration(classes = {ApiTestConfig.class})
 @Transactional
 public class MypageApiControllerTest {
 
@@ -65,18 +65,11 @@ public class MypageApiControllerTest {
 	public void givenUserInfo_whenGetRequest_thenIsStatusOk() throws Exception {
 		
 		//given
-		SessionUser user = SessionUser.builder()
-				.no(8)
-				.addressNo(8)
-				.email("wlgypark@gmail.com")
-				.name("박지효")
-				.nickname("짜릿한슬라임")
-				.marketkeeperStep('i')
-				.profileImg("profile.jpg")
-				.build();
+		
+		SessionUser user2 = mock(SessionUser.class);
 		
 		//when
-		when(apiController.getMyRecipes(user)).thenReturn(rcpList);
+		when(apiController.getMyRecipes(user2)).thenReturn(rcpList);
 		
 		//then
 		mockMvc.perform(get("/mypage/ajax/myrecipes")).andExpect(status().isOk()).andDo(print());
@@ -101,5 +94,34 @@ public class MypageApiControllerTest {
 				.characterEncoding("UTF-8")).andExpect(status().isOk())
 				.andDo(print());
 		
+	}
+	
+	@Test
+	public void givenAddress_whenPutRequest_thenIsTestPassed() throws Exception {
+		
+		//given
+		Address addrObj = Address.builder()
+		.sido("서울")
+		.gugun("마포구")
+		.dong("마장동")
+		.lat(123.00)
+		.lng(123.00)
+		.build();
+		
+		addrObj.setNo(12);
+		
+		//then
+		mockMvc.perform(put("/mypage/ajax/update_address").contentType(MediaType.APPLICATION_JSON).content(asJsonString(addrObj)))
+		.andExpect(status().isOk()).andDo(print());
+		
+	}
+	
+	
+	public static String asJsonString(Object obj) {
+		try {
+			return new ObjectMapper().writeValueAsString(obj);
+		} catch (Exception e) {
+			throw new RuntimeException();
+		}
 	}
 }
